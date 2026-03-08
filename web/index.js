4868722c444b036385ab1658316ac0cac8518f7a
Gian <85060753+notgian@users.noreply.github.com>
2026-03-08 15:47:02 +0800

    created initial structure for serving webpages
    also created the a preview for how handlebars would work for the homepage (index.hbs). The HTML folder will be deleted when we fully migrate all the folders

--- web/index.js
+++ web/index.js
@@ -1,16 +1,76 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const http = require ('http')
+const express = require('express');
+const hbs = require('express-handlebars');
+const http = require('http');
+const axios = require('axios');
+const Handlebars = require('handlebars');
 
-const port = process.env.WEB_PORT
-const app = express()
+const APP_PORT = process.env.WEB_PORT;
+const API_HOSTNAME = process.env.API_HOSTNAME;
+const API_PORT = process.env.API_PORT;
+const API_URL = `http://${API_HOSTNAME}:${API_PORT}/api/v1/`;
 
-app.engine('hbs', hbs.engine({extname:'hbs'}))
-app.set('view engine', 'hbs')
+// Handlebars Helpers
 
-app.route('/')
-    .get( (req, res) => {
-        res.render('index.hbs', { title: "test page b" } )
-    })
+// Handlebars.registerHelper('loop', (n, block) => {
+//     var cumulHTML = '';
+//     for(var i = 0; i < n; i++)
+//         cumulHTML += block.fn(i);
+//     return cumulHTML;
+// });
 
-app.listen(port)
+Handlebars.registerHelper('renderStarsHTML', function(rating) {
+    let innerText
+    if (rating === -1) {
+        innerText = "No ratings";
+    } else {
+        const roundedRating = Math.round(rating);
+        innerText = "★".repeat(roundedRating) + "☆".repeat(5 - roundedRating);
+    }
+
+    return `<span class="stars${rating == -1 ? '-no-ratings' : ''}">${innerText}</span>`
+});
+
+// Application Proper
+const app = express();
+app.engine('hbs', hbs.engine({extname:'hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static('./public'));
+
+// Homepage
+app.get('/', async (req, res) => {
+    let rstrreq = (await axios.get(API_URL+'establishments'));
+
+    let establishments;
+    if (rstrreq.status != 200)
+        establishments = new Array();
+    else if (rstrreq.data.status != 200)
+        establishments = new Array();
+    else {
+        establishments = rstrreq.data.data;
+    }
+
+    let renderData = {
+        title: '6-7-ate-9 | Home',
+        establishments: establishments,
+        css: [
+            'css/style.css',
+            'css/home.css'
+        ],
+        js: [
+            'js/script.js'
+        ]
+    }
+    
+    res.render('index.hbs', renderData )
+})
+
+app.get('/profile')
+// app.get('/profile/:id')
+
+
+app.use( (req, res, next) => {
+    // Replace with 404 page
+    res.status(404).send('Error 404 Not Found!')
+});
+
+app.listen(APP_PORT)
